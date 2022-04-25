e6869ac5813b498bcb0721c9e1296668c604d36e
fredsh2k <38362977+fredsh2k@users.noreply.github.com>
2022-04-25 16:56:12 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,35 +1,22 @@
 #!/bin/sh
 
-apt() {
-    apt-get update 
-    apt-get install -y zsh fzf
-    apt-get autoremove -y
-    apt-get clean -y
-}
+# apt 
+echo "updating apt, installing packages, cleaning apt"
+apt update 
+apt install -y zsh fzf
+apt autoremove -y
+apt clean -y
 
-zshrc() {
-    echo "==========================================================="
-    echo "             cloning zsh-autosuggestions                   "
-    echo "-----------------------------------------------------------"                    
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# zsh
+echo "installing zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-    echo "==========================================================="
-    echo "             cloning zsh-syntax-highlighting               "
-    echo "-----------------------------------------------------------"
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+echo "installing zsh-syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-    echo "==========================================================="
-    echo "             cloning spaceship-prompt                      "
-    echo "-----------------------------------------------------------"
-    git clone https://github.com/spaceship-prompt/spaceship-prompt.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt --depth=1
-    ln -s ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt/spaceship.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship.zsh-theme
+echo "installing spaceship-prompt"
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt --depth=1
+ln -s ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt/spaceship.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship.zsh-theme
 
-    echo "==========================================================="
-    echo "                 Copy .zshrc to HOME                       "
-    echo "-----------------------------------------------------------"
-    cat .zshrc > $HOME/.zshrc
-}
-
-apt
-
-zshrc
+echo "copy .zsrhc to HOME"
+cat .zshrc > $HOME/.zshrc
