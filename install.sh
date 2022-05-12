5df5056c4e519ff4b777f281a9821b572d29a713
fredsh2k <38362977+fredsh2k@users.noreply.github.com>
2022-05-12 15:03:43 +0300

    lunarvim

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-# apt 
+# apt
 echo "updating apt, installing packages, cleaning apt"
-sudo apt update 
-sudo apt install -y zsh fzf vim
+sudo apt update
+sudo apt install -y zsh fzf
 sudo apt autoremove -y
 sudo apt clean -y
 
@@ -17,6 +17,25 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 echo "installing spaceship-prompt"
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt --depth=1
 ln -s ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt/spaceship.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship.zsh-theme
+
+echo "install neovim"
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage --appimage-extract
+./squashfs-root/AppRun --version
+
+sudo mv squashfs-root /
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+
+echo "install lunarvim"
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt install -y nodejs
+sudo apt install -y pip
+
+bash <(curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs) -y
+source $HOME/.cargo/env
+
+yes | bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) # answer no to all prompts
 
 echo "copy .zsrhc to HOME"
 cat .zshrc > $HOME/.zshrc
