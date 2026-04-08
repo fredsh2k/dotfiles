07027a645b3d93dd40ced3d6f706961f478252b1
fredsh2k <fredsh2k@github.com>
2026-04-08 22:07:20 +0300

    fix: remove unused render-markdown.nvim and example.lua from lazyvim config

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,11 +1,5 @@
 return {
   {
-    "MeanderingProgrammer/render-markdown.nvim",
-    opts = {
-      enabled = false, -- off by default, toggle with :RenderMarkdown toggle
-    },
-  },
-  {
     "mfussenegger/nvim-lint",
     optional = true,
     opts = {
